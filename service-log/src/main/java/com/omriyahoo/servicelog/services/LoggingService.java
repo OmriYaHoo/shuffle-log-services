a3b68b6ec72577072a149bb50eebb05b258883a9
omri.ratson <omri.ratson@nice.com>
2023-02-19 11:02:45 +0200

    minor changes

--- service-log/src/main/java/com/omriyahoo/servicelog/services/LoggingService.java
+++ service-log/src/main/java/com/omriyahoo/servicelog/services/LoggingService.java
@@ -4,12 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 @Slf4j
 @Service
+@RefreshScope
 @ConfigurationProperties(prefix = "logging-service")
 public class LoggingService {
 
